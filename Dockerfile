05b11fe1b7715f67f18806c33abc159755ed685f
Matan Tsuberi <tsuberim@gmail.com>
2018-11-24 00:28:02 +0200

    use web3 35

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,9 @@
-FROM node:8
+FROM mhart/alpine-node:10.13.0
+
+# install git, python, make
+RUN apk update \
+    && apk upgrade \
+    && apk add --no-cache bash git openssh python make g++
 
 WORKDIR /app
 COPY . .
--- release.sh
+++ release.sh
@@ -8,14 +8,14 @@ npm run migrate -- "$@"
 # set version
 echo "Setting version..."
 node set-version.js
-# publish npm
-echo "Publishing to npm..."
-npm login
-npm publish
 # publish docker
 echo "Publishing to dockerhub..."
 npm run docker:build
 npm run docker:push
+# publish npm
+echo "Publishing to npm..."
+npm login
+npm publish
 # commit addresses
 echo "Commiting changes..."
 git add -A && git commit -m "release $(cat package.json | jq -r '.version')"
