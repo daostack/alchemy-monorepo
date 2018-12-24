704d8a71f43cf390e7202a679c7b1a6999fa1aab
Ben Kaufman <ben.kaufman10@gmail.com>
2018-12-24 11:29:59 +0200

    Fixed linter issue

--- setup-env.js
+++ setup-env.js
@@ -1,8 +1,8 @@
-const path = require('path');
+const path = require('path')
 
 async function main () {
-  const subgraphRepo = path.resolve('./node_modules/@daostack/subgraph');
-  console.log(await require(`${subgraphRepo}/ops/setup-env`)());
+  const subgraphRepo = path.resolve('./node_modules/@daostack/subgraph')
+  console.log(await require(`${subgraphRepo}/ops/setup-env`)())
 }
 
-main();
+main()
