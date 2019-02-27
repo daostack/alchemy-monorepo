5788d137af93a89693524ccf5091fa4c002295cd
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-02-27 21:03:16 +0100

    add option to have more settings in ipfs provider

--- test/proposal-create.spec.ts
+++ test/proposal-create.spec.ts
@@ -116,6 +116,7 @@ describe('Create a ContributionReward proposal', () => {
     })
 
   })
+
   it('handles the fact that the ipfs url is not set elegantly', async () => {
     const arcWithoutIPFS = new Arc({
       graphqlHttpProvider,
