59c1862d4d7eec2e9bcdf1d73817cee01505f843
orenyodfat <orenyodfat@users.noreply.github.com>
2019-07-24 13:11:39 +0300

    add Schemes filed to migrate.dao output (#172)
    * add Schemes filed to migrate.dao output
    
    * lint

--- migrate-dao.js
+++ migrate-dao.js
@@ -87,7 +87,12 @@ async function migrateDAO ({ web3, spinner, confirm, opts, migrationParams, logT
     migrationParams.founders
   ]
 
-  let avatar, daoToken, reputation, Controller, controller
+  let avatar
+  let daoToken
+  let reputation
+  let Controller
+  let controller
+  let Schemes = { }
 
   if (migrationParams.useDaoCreator === true) {
     spinner.start('Creating a new organization...')
@@ -423,6 +428,7 @@ async function migrateDAO ({ web3, spinner, confirm, opts, migrationParams, logT
     schemes.push(reputationFromToken.options.address)
     params.push('0x0000000000000000000000000000000000000000000000000000000000000000')
     permissions.push('0x00000001')
+    Schemes.ReputationFromToken = reputationFromToken.options.address
   }
 
   for (const schemeName in migrationParams.CustomSchemes) {
@@ -480,6 +486,7 @@ async function migrateDAO ({ web3, spinner, confirm, opts, migrationParams, logT
     schemes.push(schemeContract.options.address)
     params.push(schemeParamsHash)
     permissions.push(scheme.permissions)
+    Schemes[schemeName] = schemeContract.options.address
   }
 
   if (migrationParams.useDaoCreator === true) {
@@ -500,7 +507,8 @@ async function migrateDAO ({ web3, spinner, confirm, opts, migrationParams, logT
       Avatar: avatar.options.address,
       DAOToken: daoToken.options.address,
       Reputation: reputation.options.address,
-      Controller
+      Controller,
+      Schemes
     }
   )
   let migration = { 'dao': previousMigration.dao || {} }
@@ -509,7 +517,8 @@ async function migrateDAO ({ web3, spinner, confirm, opts, migrationParams, logT
     Avatar: avatar.options.address,
     DAOToken: daoToken.options.address,
     Reputation: reputation.options.address,
-    Controller
+    Controller,
+    Schemes
   }
   return migration
 }
