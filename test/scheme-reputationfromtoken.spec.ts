f609c668ad79a2b572b231eb28a0b66130950ca8
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-07-26 13:43:12 +0200

    fix lint error

--- test/scheme-reputationfromtoken.spec.ts
+++ test/scheme-reputationfromtoken.spec.ts
@@ -14,14 +14,17 @@ describe('Scheme', () => {
     const scheme = new Scheme({
         address: '0x1224',
         dao: dao.id,
-        id: '0x123455',
+        id: '0.x123455',
         name: 'ReputationFromToken',
         paramsHash: '0x124'
     }, arc)
 
-    const amount = await scheme.ReputationFromToken.redemptionAmount(arc.web3.eth.defaultAccount)
-    expect(amount).toEqual(0)
-    await scheme.ReputationFromToken.redeem(arc.web3.eth.defaultAccount)
+    expect(scheme.ReputationFromToken).not.toBeFalsy()
+    if (scheme.ReputationFromToken) {
+      const amount = await scheme.ReputationFromToken.redemptionAmount(arc.web3.eth.defaultAccount)
+      expect(amount).toEqual(0)
+      await scheme.ReputationFromToken.redeem(arc.web3.eth.defaultAccount)
+    }
   })
 
 })
