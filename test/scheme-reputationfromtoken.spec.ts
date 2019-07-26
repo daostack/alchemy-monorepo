817c977721e6102a82c8ae32fb0010863a666082
Jelle Gerbrandy <jellegerbrandy@gmail.com>
2019-07-26 12:39:48 +0200

    fix ts error in test

--- test/scheme-reputationfromtoken.spec.ts
+++ test/scheme-reputationfromtoken.spec.ts
@@ -1,23 +1,17 @@
-import { ReputationFromTokenScheme } from '../src/schemes/reputationFromToken'
+import { Scheme } from '../src/scheme'
 import { getTestDAO, newArc } from './utils'
 
-jest.setTimeout(20000)
-
+jest.setTimeout(60000)
 /**
  * Scheme test
  */
 describe('Scheme', () => {
 
   it.skip('Test the whole flow', async () => {
-    // deploy the FixedReputationAllocation contrat
-    // const contractInfo =   require(`@daostack/arc/build/contracts/ReputationFromToken.json`)
-    //
-    // const contract = new arc.web3.eth.Contract(contractInfo.abi)
-    // await contract.deploy({ data: contractInfo.bytecode})
     const arc = await newArc()
     const dao = await getTestDAO()
 
-    const scheme = new ReputationFromTokenScheme({
+    const scheme = new Scheme({
         address: '0x1224',
         dao: dao.id,
         id: '0x123455',
@@ -25,9 +19,9 @@ describe('Scheme', () => {
         paramsHash: '0x124'
     }, arc)
 
-    const amount = await scheme.redemptionAmount(arc.web3.eth.defaultAccount)
+    const amount = await scheme.ReputationFromToken.redemptionAmount(arc.web3.eth.defaultAccount)
     expect(amount).toEqual(0)
-
+    await scheme.ReputationFromToken.redeem(arc.web3.eth.defaultAccount)
   })
 
 })
