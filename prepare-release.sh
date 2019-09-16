afa378480dde5850f12f18f431b48d9d667b51bc
benk10 <ben@daostack.io>
2019-09-16 11:43:10 +0300

    V24 3 (#191)
    * release 0.0.1-rc.24-v3
    
    * Add rinkeby

--- prepare-release.sh
+++ prepare-release.sh
@@ -32,17 +32,17 @@ npm run prune-arc-build -- "$@"
 echo "Migrating ganache..."
 npm run migrate -- --force "$@"
 done
-if [ -z "$kovan_private_key" ]; then
+if [ ! -z "$kovan_private_key" ]; then
 # migrate kovan
 echo "Migrating kovan..."
 npm run migrate -- --gasPrice 10 --provider $kovan_provider --private-key $kovan_private_key "$@"
 fi
-if [ -z "$rinkeby_private_key" ]; then
+if [ ! -z "$rinkeby_private_key" ]; then
 # migrate rinkeby
 echo "Migrating rinkeby..."
 npm run migrate -- --gasPrice 10 --provider $rinkeby_provider --private-key $rinkeby_private_key "$@"
 fi
-if [ -z "$mainnet_private_key" ]; then
+if [ ! -z "$mainnet_private_key" ]; then
 # migrate mainnet
 echo "Migrating mainnet..."
 npm run migrate -- --gasPrice 30 --provider $mainnet_provider --private-key $mainnet_private_key "$@"
