cc7544d57a972eff5174dbf5ac8354c22c811f1e
benk10 <ben@daostack.io>
2019-09-16 15:02:52 +0300

    Fix prepare-release contract verification script (#192)

--- prepare-release.sh
+++ prepare-release.sh
@@ -70,8 +70,8 @@ npm run verify.build
 
 echo "Verifying..."
 read -n 1 -s -r -p "Press any key to verify contracts on kovan"
-npm run verify -n kovan -p $kovan_provider
+npm run verify -- -n kovan -p $kovan_provider
 read -n 1 -s -r -p "Press any key to verify contracts on rinkeby"
-npm run verify -n rinkeby -p $rinkeby_provider
+npm run verify -- -n rinkeby -p $rinkeby_provider
 read -n 1 -s -r -p "Press any key to verify contracts on mainnet"
-npm run verify -n mainnet -p $mainnet_provider
+npm run verify -- -n mainnet -p $mainnet_provider
