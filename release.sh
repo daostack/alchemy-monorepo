3d6d0f9727361dc33ace171b93aa72a1840f3020
jelle <jellegerbrandy@gmail.com>
2020-06-01 17:08:31 +0200

    Package version 0.2.70

--- release.sh
+++ release.sh
@@ -3,12 +3,17 @@ set -e # fail on error
 echo "Creating a new release"
 npm ci
 package_version=$(cat package.json | jq -r '.version')
-echo "publish to npm"
-npm publish
+
 # tag on github
 echo "create tag ${package_version}"
-git ci -a -m "Package version ${package_version}"
+git ci -a -m "Package version ${package_version}" --allow-empty
 git tag -a $package_version -m "Release of version $package_version"
 git push --tags
+
+# publish to npm
+echo "publish to npm"
+npm publish --access public
+
 # done
-echo "Done!"
+echo "Done"
+echo "Please update the releases tab here: https://github.com/daostack/arc.js/releases"
