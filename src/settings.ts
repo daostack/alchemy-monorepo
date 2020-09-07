b4dbae14c55d924115c9e6f279def302ed0d0b4d
Doug Kent <dkent600@users.noreply.github.com>
2020-09-07 11:55:24 -0400

    add redeemer version (#525)
    * add redeemer version
    
    * remove trailing comma
    
    * bump version

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,9 @@
 // the version of the redeemer contract to use.
 // we can specify multiple versions, and try to find them in the order given
 export const REDEEMER_CONTRACT_VERSIONS = [
-    '0.0.1-rc.37',
-    '0.0.1-rc.36'
+  '0.0.1-rc.44',
+  '0.0.1-rc.37',
+  '0.0.1-rc.36'
 ]
 // the version of the Reputation, and token contract instances
 export const REPUTATION_CONTRACT_VERSION = '0.0.1-rc.19'
