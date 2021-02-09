8d6b5c68cd51434245dbd8139c3cec3827043f82
benk10 <ben.kaufman10@gmail.com>
2021-02-09 15:21:11 +0200

    Travis attempt 1

---
+++ packages/infra/test-ci.sh
@@ -0,0 +1,10 @@
+npm ci
+rm -rf build/ # remove any remaining artifacts from a previous build
+npx buidler --version
+npx buidler clean
+
+npx buidler test
+npx buidler check
+npx eslint .
+# TODO: If branch is master
+npm run coveralls
---
+++ packages/subgraph/test-ci.sh
@@ -0,0 +1,23 @@
+docker --version
+docker-compose --version
+
+docker-compose up -d graph-node
+docker-compose logs graph-node
+
+echo 'npm ci:' && echo -en 'travis_fold:start:script.1\\r'
+npm ci
+echo -en 'travis_fold:end:script.1\\r'
+
+  # deploy contracts etc
+npm run deploy
+
+echo 'Debug info:' && echo -en 'travis_fold:start:script.2\\r'
+docker images --digests | grep graph-node
+docker-compose exec graph-node graph-node --version
+  # - docker-compose logs graph-node
+echo -en 'travis_fold:end:script.2\\r'
+sleep 120
+
+npm run lint
+npm run jest
+npm run docker:stop
