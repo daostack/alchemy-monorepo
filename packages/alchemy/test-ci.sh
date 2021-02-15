efbe1e6d0bc7d0e2ffb3d2e3f2337aa2495528b8
Roie Natan <roie.natan@gmail.com>
2021-02-15 13:50:40 +0200

    .

--- packages/alchemy/test-ci.sh
+++ packages/alchemy/test-ci.sh
@@ -1,7 +1,5 @@
-npm i -g eslint
-npm i -g jest
-npm ci
 sudo apt-get install -y libsecret-1-dev curl
+npm ci
 docker-compose logs ganache
 docker-compose logs graph-node
 # sleep a bit to make sure the subgraph is ready
