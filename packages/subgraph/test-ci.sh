4fc4290dceba7a4ab43f975f6e9a95f605d8353f
jelle <jellegerbrandy@gmail.com>
2021-03-02 10:29:19 +0100

    run subgraph tests within repo

--- packages/subgraph/test-ci.sh
+++ packages/subgraph/test-ci.sh
@@ -5,11 +5,11 @@ docker-compose --version
 docker-compose up -d graph-node
 docker-compose logs graph-node
 
-echo 'npm ci:' && echo -en 'travis_fold:start:script.1\\r'
-npm ci
-echo -en 'travis_fold:end:script.1\\r'
+#echo 'npm ci:' && echo -en 'travis_fold:start:script.1\\r'
+#npm ci
+#echo -en 'travis_fold:end:script.1\\r'
 
-  # deploy contracts etc
+# deploy contracts etc
 npm run deploy
 
 echo 'Debug info:' && echo -en 'travis_fold:start:script.2\\r'
