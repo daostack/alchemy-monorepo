0a1f3c88a75723d669bb7922d157835b315cc4de
Roie Natan <roie.natan@gmail.com>
2021-03-04 10:08:08 +0200

    update selenium driver ; add chrome flags

--- packages/alchemy/test/integration/wdio.conf.ts
+++ packages/alchemy/test/integration/wdio.conf.ts
@@ -16,7 +16,7 @@ const config: WebdriverIO.Config = {
       maxInstances: 1,
       browserName: "chrome",
       "goog:chromeOptions": {
-        args: ["window-size=1920,1080"],
+        args: ["window-size=1920,1080", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--disable-extensions"],
       },
     },
   ],
